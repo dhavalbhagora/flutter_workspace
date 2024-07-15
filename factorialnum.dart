6176ba59025efaec9398fe6286ea9fb1494e233e
Dhaval Bhagora <145954819+dhavalbhagora@users.noreply.github.com>
2024-07-15 09:45:45 +0530

    commit

---
+++ factorialnum.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+
+void main() {
+  print("enter your number");
+  int num = int.parse(stdin.readLineSync().toString());
+  int fact=1;
+ for(int i=num;i>=1;i--){
+    fact*=i;
+
+ }
+ print(fact);
+}
---
+++ whileex.dart
@@ -0,0 +1,10 @@
+void main() {
+  int num = 51;
+  int num2 = 60;
+
+
+     while (num <= num2) {
+    print(num);
+    num++;
+  }
+}
