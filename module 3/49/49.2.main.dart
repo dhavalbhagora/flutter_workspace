80daf3099f52ec5c1be196696b4056d0397851a2
Dhaval Bhagora <145954819+dhavalbhagora@users.noreply.github.com>
2024-08-23 15:05:36 +0530

    commit

--- module 3/49/49.2.main.dart
+++ module 3/49/49.2.main.dart
@@ -2,7 +2,5 @@ import 'package:flutter/material.dart';
 import 'package:module_3/module_3_1_2.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: module_3_1_2(),
-  ));
+  runApp(MaterialApp(home: OeschinenLakeCampground()));
 }
